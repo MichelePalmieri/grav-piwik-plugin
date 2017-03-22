b77160c047f6c2908538f16dbc6eb24ecc9160aa
Michele Palmieri <mik.palmieri@katamail.com>
2017-03-22 22:14:45 +0100

    Update piwik.php
    https://github.com/MichelePalmieri/grav-piwik-plugin/pull/5

--- piwik.php
+++ piwik.php
@@ -34,9 +34,9 @@ class piwikPlugin extends Plugin
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u=\"{$sitePiWikURL}/\";
+    var u=\"//{$sitePiWikURL}/\";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', {$siteId}]);
+    _paq.push(['setSiteId', '{$siteId}']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
